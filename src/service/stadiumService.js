2f316d12229de9b01a1279f762cf4fa076f8368f
SHARIFAALDOSERI <sharifaaldoseri5@gmail.com>
2025-04-13 19:21:17 +0300

    Update stadiumService.js

--- src/service/stadiumService.js
+++ src/service/stadiumService.js
@@ -1,41 +1,37 @@
 import axios from "axios";
 
-const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/stadium`;
+const BASE_URL = ${import.meta.env.VITE_BACKEND_URL}/stadium;
+  
+  const api = axios.create({
+      baseURL: BASE_URL 
+  });
 
-const api = axios.create({
-  baseURL: BASE_URL,
+  api.interceptors.request.use(config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = Bearer ${token};
+    }
+    return config;
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-// Get all stadiums
 const getAllStadiums = async () => {
   try {
-    const res = await api.get("/");
-    console.log("res", res.data);
-    return res. data;
+      const res = await api.get('/'); 
+      console.log('res', res.data);
+      return res.data;
   } catch (error) {
-    console.log("Error fetching data:", error);
-    return []; // Return empty array in case of error
+      console.log('Error fetching data:', error);
   }
 };
 
-// Create a new stadium
-const createStadium = async (formData) => {
-  try {
-    const res = await api.post("/", formData);
-    console.log("Response from backend:", res.data);
-    return res. data;
-  } catch (err) {
-    console.log("Error creating stadium:", err.message);
-    throw err; // Rethrow the error for the caller to handle if needed
+async function createStadium(formData){
+  try{
+      const res = await api.post(/,formData)
+      console.log("Response from backend:", res.data);
+      return res.data
+  }catch(err){
+      console.log(err.message)
   }
-};
+}
 
-export { getAllStadiums, createStadium };
+export  {getAllStadiums,createStadium}; 
